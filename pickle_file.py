be655bc9e71ac9b4d169ca19d5d77b0634acf37f
SebaJoha <sejh21@student.bth.se>
2024-05-10 16:26:33 +0200

    Tagit bort lite mellanrum och har samlat all i en mapp

--- pickle_file.py
+++ pickle_file.py
@@ -3,27 +3,31 @@ import sys
 import os
 import platform
 
+local_dir = "Pickles"
 operation_system = platform.system()
 if operation_system == "Darwin":
     operation_system = "MacOS"
 python_version = sys.version.split()[0]
 
-print(operation_system)
-print(python_version)
-
 test_case_dict = {"int": 3786587, "float": float(10/11), "float_nan": float('nan'), "float_inf": float('inf'), "complex": complex(5,3), 
                   "string": "test\n\t\r", "f_string": f"fstring", "r_string": r"rstring", "b_string": b"bstring",
                   "tuple": (1, (2)), "frozenset": frozenset([1,2,3,4]), "range": range(8), 
-                  "bytes": bytes(5), "False": False, "None": None, "list": [1, "2", True, ((4, 5), 6)], "list in list": [[], [[]]]}
+                  "bytes": bytes(5), "False": False, "None": None, "list": [1, "2", True, ((4, 5), 6)], "list_in_list": [[], [[]]]}
 
 
-directory = f"{operation_system}"
+directory = f"{local_dir}"
 try:
     os.mkdir(directory) 
 except:
     pass
 
-directory = f"{operation_system}/{python_version}"
+directory = f"{local_dir}/{operation_system}"
+try:
+    os.mkdir(directory) 
+except:
+    pass
+
+directory = f"{local_dir}/{operation_system}/{python_version}"
 try:    
     os.mkdir(directory) 
 except:
