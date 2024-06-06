f2bc3a7acff44fb31d360ffa553d91084cc9a8a6
SebaJoha <sejh21@student.bth.se>
2024-06-06 11:17:58 +0200

    Lagt in fler datatyper

--- pickle_file.py
+++ pickle_file.py
@@ -9,11 +9,27 @@ if operation_system == "Darwin":
     operation_system = "MacOS"
 python_version = sys.version.split()[0]
 
+class Class1:
+    x = 5
+    string = "Hello"
+
+class Class2:
+    nan = float('nan')
+
+class Class3:
+    child1 = Class1()
+    child2 = Class2()
+
+class Class4:
+    dictionary = {"child3": Class1(), "child4": Class2()}
+
 test_case_dict = {"int": 3786587, "float": float(10/11), "float_nan": float('nan'), "float_inf": float('inf'), "complex": complex(5,3), 
                   "string": "test\n\t\r", "f_string": f"fstring", "r_string": r"rstring", "b_string": b"bstring",
                   "tuple": (1, (2)), "frozenset": frozenset([1,2,3,4]), "range": range(8), 
                   "bytes": bytes(5), "False": False, "None": None, "list": [1, "2", True, ((4, 5), 6)], "list_in_list": [[], [[]]], 
-                  "dict": {"value_here": 1, "dict_in_dict_in_dict": {"Not_empty": "David är elak mot mig :("}}}
+                  "dict": {"value_here": 1, "dict_in_dict_in_dict": {"Not_empty": "David är elak mot mig :("}}, "set": {"Sebbe", "David", "Albin"},
+                  "memory_view":  memoryview(bytes(5)), "byte_array": bytearray("We are not alergic to peanuts", 'utf-8'),
+                  "class1": Class1(), "class2": Class2(), "class3": Class3(), "class4": Class4()}
 
 
 directory = f"{local_dir}"
